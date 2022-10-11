9346269f3379bff546dc4bb181c6b2963ca89bb9
jspsegarra <jspsegarra86@gmail.com>
2022-10-11 19:27:36 +0200

    Supabase completed 🥵

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,31 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <q-layout id="app" view="hHh lpR fFf">
-
-    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar class="bar-header">
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="/block-favicon.png">
-          </q-avatar>
-          The most interesting Blockchains
-        </q-toolbar-title>
-      </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/components/Home" label="Home" />
-        <q-route-tab to="/components/What is" label="What is...?" />
-        <q-route-tab to="/components/AboutMe" label="About me" />
-      </q-tabs>
-    </q-header>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
-    <q-footer reveal elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="/block-favicon.png">
-          </q-avatar>
-          <div>The most interesting Blockchains</div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
-
-  </q-layout>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Profile v-if="store.user" />
+    <Auth v-else />
+  </div>
 </template>
 
-<style>
-.bar-header{
-  
-}
+<script>
+  import { store } from './store'
+  import { supabase } from './supabase'
+  import Auth from './components/Auth.vue'
+  import Profile from './components/Profile.vue'
 
-</style>
+  export default {
+    components: {
+      Auth,
+      Profile,
+    },
+
+    setup() {
+      store.user = supabase.auth.user()
+      supabase.auth.onAuthStateChange((_, session) => {
+        store.user = session.user
+      })
+
+      return {
+        store,
+      }
+    },
+  }
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+    <q-layout id="app" class="bar-header" view="hHh lpR fFf">
+  
+      <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+        <q-toolbar class="bar-header">
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="/block-favicon.png">
+            </q-avatar>
+            The most interesting Blockchains
+          </q-toolbar-title>
+        </q-toolbar>
+  
+        <q-tabs align="left">
+          <q-route-tab to="/components/Home" label="Home" />
+          <q-route-tab to="/components/What is" label="What is...?" />
+          <q-route-tab to="/components/AboutMe" label="About me" />
+        </q-tabs>
+      </q-header>
+  
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+  
+      <q-footer reveal elevated class="bg-grey-8 text-white">
+        <q-toolbar>
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="/block-favicon.png">
+            </q-avatar>
+            <div>The most interesting Blockchains</div>
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+  
+    </q-layout>
+  </template>
+  
+  <style>
+  .bar-header{
+    background-color: blue;
+  }
+  
+  </style>
