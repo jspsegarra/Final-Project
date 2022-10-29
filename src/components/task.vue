f2fd26b2e6746eec3ceb3437e3ffbce5aedefa5d
jspsegarra <jspsegarra86@gmail.com>
2022-10-29 11:30:54 +0200

    Add - delete tasks mechanism

--- src/components/task.vue
+++ src/components/task.vue
@@ -1,23 +1,32 @@
-<script setup> 
+<script setup>
 import { ref } from 'vue'
 import router from '../router';
 import { useTaskStore } from '../stores/task.js'
-const taskName = ref('')
 const isComplete = ref('')
-const userId = ref('')
+const title = ref('')
 const taskStore = useTaskStore()
-async function handleSubmit (){
+/* async function handleSubmit (){
     try {
         await taskStore.fetchTasks(taskName.value, isComplete.value, userId.value)
         console.log('yehaaa!')
-        /* router.push({path:'/home'}) */
+         router.push({path:'/home'}) 
     }
-    catch(error) {console.log ('fallamos', error)}
-}
+    catch(error) {console.log ('fallamos', error)} 
+} */
 </script>
 
 <template>
-
+    <h1 class="beautyTitle">Please update your tasks here!</h1>
+    <form @submit.prevent="handleSubmit" class="newTask input-group" method="post">
+        <input type="text" class="input-group-field" v-model="title" placeholder="New task">
+        <input type="checkbox" v-model="is_complete">
+        <span class="input-group-button">
+            <button class="button" type="submit">
+                <i class="fa-solid fa-file-circle-plus"></i> Add </button>
+        </span>
+    </form>
+    <button @click="">
+        <i class="fa-solid fa-xmark">Delete</i></button>
 </template>
 
 <style>
--- src/stores/task.js
+++ src/stores/task.js
@@ -8,13 +8,28 @@ export const useTaskStore = defineStore('tasks', {
     tasks: null
   }),
   actions: {
-    async fetchTasks () {
+    async fetchTasks() {
       const { data: tasks } = await supabase
         .from('tasks')
         .select('*')
         .order('id', { ascending: false });
       this.tasks = tasks;
+    },
+    async newTask(title, complete) {
+      const { error } = await supabase
+        .from('tasks')
+        .insert({
+          title: title,
+          is_complete: complete,
+        })
+    },
+    async deleteTask(title) {
+      const { error } = await supabase
+        .from('tasks')
+        .delete({
+          title: title,
+        })
+        .eq('title')
     }
   }
 });
-
